b2d23e86a173058c9f0ef07a136ae0f562734094
RizvanEn <rizvanenego@gmail.com>
2025-02-04 16:04:26 +0530

    redirect route impletyemented

--- routes/shortUrlRoutes.js
+++ routes/shortUrlRoutes.js
@@ -8,7 +8,7 @@ const shortUrl = express.Router();
 shortUrl.post("/shorten", urlValidator, async (req, res) => {
   const { longUrl, topic } = req.body;
   try {
-    if (req.isAuthenticated()) {
+    
       const uniqueStrings = new Set();
       const generateUniqueString = () => {
         let str;
@@ -28,18 +28,43 @@ shortUrl.post("/shorten", urlValidator, async (req, res) => {
         longUrl,
         alias: generateUniqueString(),
         topic: topic ? topic : "",
-        user_mail:req.user.emails[0].value
+        user_mail: req.user.emails[0].value,
       };
-    }
-    const shortUrl = await UrlModel.create(service);
-    res
-      .status(201)
-      .send({ message: "Short url created sucessfully", shortUrl });
+      const shortUrl = await UrlModel.create(service);
+     
+      res
+        .status(201)
+        .send({ shortUrl:`http://localhost:3000/api/shorten/${shortUrl.alias}`});
+    
   } catch (error) {
     res
       .status(500)
       .send({ message: "Error Creating short url", error: error.message });
   }
 });
+
+// GET Route to Find Data by Alias
+shortUrl.get("/shorten/:alias", async (req, res) => {
+  const { alias } = req.params; // Get the alias from the URL
+
+  try {
+    // Find the URL in the database using the alias
+    const shortUrlData = await UrlModel.findOne({ alias });
+
+    if (!shortUrlData) {
+      return res.status(404).send({ message: "Short URL not found" });
+    }
+
+    res.redirect(shortUrlData.longUrl);
+  } catch (error) {
+    res.status(500).send({
+      message: "Error retrieving short URL",
+      error: error.message,
+    });
+  }
+});
+
+
+
 
 export default shortUrl;
