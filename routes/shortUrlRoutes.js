bd308abf7f7ec79ac5e5a2695236bd500bec5fe6
RizvanEn <rizvanenego@gmail.com>
2025-02-05 17:57:26 +0530

    url updated

--- routes/shortUrlRoutes.js
+++ routes/shortUrlRoutes.js
@@ -10,7 +10,7 @@ import { UAParser } from 'ua-parser-js';
 import dotenv from 'dotenv'
 dotenv.config()
 
- const BASE_URL=process.env.BASE_URL;
+ const URL=process.env.BASE_URL;
 
 
 
@@ -47,7 +47,7 @@ shortUrl.post("/shorten", urlValidator, async (req, res) => {
      
       res
         .status(201)
-        .send({ shortUrl:`${BASE_URL}/api/shorten/${shortUrl.alias}`});
+        .send({ shortUrl:`${URL}/api/shorten/${shortUrl.alias}`});
     
   } catch (error) {
     res
@@ -243,7 +243,7 @@ shortUrl.get("/analytics/topic/:topic", async (req, res) => {
       const uniqueIPs = new Set(urlAnalytics.map(data => data.ipAddress));
 
       return {
-        shortUrl: `${BASE_URL}/shorten/${url.alias}`, 
+        shortUrl: `${URL}/shorten/${url.alias}`, 
         totalClicks: urlAnalytics.length,
         uniqueUsers: uniqueIPs.size
       };
@@ -362,7 +362,7 @@ shortUrl.get("/user-urls", async (req, res) => {
   try {
     const urls = await UrlModel.find({ user_mail: email });
     const response = urls.map(url => ({
-      shortUrl: `${BASE_URL}/api/shorten/${url.alias}`,
+      shortUrl: `${URL}/api/shorten/${url.alias}`,
       clicks: url.uniqueClicks || 0, // Assuming clicks are stored
     }));
     res.json(response);
